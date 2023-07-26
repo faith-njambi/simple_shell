9abd3728b3349606612696ef1fc9292bda6d313a
Harvey W. Kis <harveykisiangani@gmail.com>
2023-07-26 21:53:14 +0300

    parser updated, main function removed from within

--- parse.c
+++ parse.c
@@ -70,39 +70,3 @@ char **strtow(char *str)
 	}
 	return (str_arr);
 }
-
-/**
- * print_tab - prints an array of string
- * @tab: array to print
- *
- * Return: Nothing
- */
-void print_tab(char **tab)
-{
-
-	int i;
-
-	for (i = 0; tab[i] != NULL; ++i)
-	{
-		printf("%s\n", tab[i]);
-	}
-}
-
-/**
- * main - checks the code
- *
- * Return: 1 if error occurs, 0 on success
- */
-int main(void)
-{
-	char **tab;
-
-	tab = strtow("		ALX School         #cisfun      ");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	return (0);
-}
