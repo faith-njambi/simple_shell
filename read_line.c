7e733e7709830f3cde4399e2dd2dee727290fbe9
Faith Njambi <njambifaith30@gmail.com>
2023-07-29 05:57:34 +0300

    command execution amd tokenization

--- read_line.c
+++ read_line.c
@@ -1,47 +1,16 @@
-#include <unistd.h>
-#include <stdio.h>
-#include "main.h"
+#include "shell.h"
 
 /**
- * readline - Reads a strings from stdin
- *
- * Return: number of chars read
+ * read_line - fetches the input stt
+ * @eof_input: return value of getline function
+ * Return: input str
  */
-int readline(void)
+char *read_line(int *eof_input)
 {
+	char *input = NULL;
+	size_t bufsize = 0;
 
-	int count = 0;
-	char buf[72];
-	char *buf_ptr;
-	int char_r = 0;
-	int char_w = 0;
+	*eof_input = getline(&input, &bufsize, stdin);
 
-
-		do {
-		write(1, "#cisfun$ ", 9);
-			char_r = read(0, buf, 72);
-			if (char_r > 0)
-			{
-				buf_ptr = buf;
-				count += char_r;
-				while (char_r > 0)
-				{
-					char_w = write(STDOUT_FILENO, buf_ptr, char_r);
-					if (char_w < 0)
-						perror("Error: ");
-					char_r -= char_w;
-					buf_ptr += char_w;
-				}
-			}
-			else if (char_r < 0)
-				perror("Read: ");
-			else
-				break;
-		} while (1);
-	return (count);
+	return (input);
 }
-/*
- *	do {
- *
- * } while (1);
- */
